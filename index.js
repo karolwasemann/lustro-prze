42011c20c6b0560d4eddc9435965dc2253bbe143
karolwasemann <kw.kwaq@gmail.com>
2021-04-26 08:09:37 +0200

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-import './style.css';
+import "./style.css";
 
+const text = document.querySelector(".text-input");
+const miror = document.querySelector(".mirror");
+
+text.addEventListener("keyup", event => {
+  let txt = event.target.value;
+  txt = [...txt].reverse().join("");
+  miror.innerText = txt;
+});
